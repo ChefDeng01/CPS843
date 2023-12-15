667cde8467926cbfb7e5764cfaf244e4062ea2dc
Gary Deng <65429541+ChefDeng01@users.noreply.github.com>
2023-12-15 14:24:34 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+
 from ultralytics import YOLO
-import cv2
+#import cv2
 import torch
 
 #setup GPU
-device = "1" if torch.cuda.is_available() else "cpu"
+device = "0" if torch.cuda.is_available() else "cpu"
 if device == "0":
     torch.cuda.set_device(0)
 
 # load yolov8 model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8m.pt')
 
-results = model.train(data="data.yaml", epochs=2,device='cuda')
+results = model.train(data="data.yaml", epochs=200)
